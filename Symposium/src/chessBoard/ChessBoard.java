40520db7b707af63a0e9b20be71ce50117bf3a90
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-04-12 15:22:16 -0400

    gui chess

--- Symposium/src/chessBoard/ChessBoard.java
+++ Symposium/src/chessBoard/ChessBoard.java
@@ -3,7 +3,7 @@ package chessBoard;
 public class ChessBoard {
 
 	BoardTile[][] board = new BoardTile[8][8];
-	String[] col = {"A", "B", "C", "D", "E", "F", "G"};
+	public static final String[] col = {"A", "B", "C", "D", "E", "F", "G", "H"};
 	String[] chessPieces = {"Rook", "Knight", "Bishop", "Queen", "King"};
 	public ChessBoard() {
 		for(int row =0; row< board.length; row++ )
--- Symposium/src/chessGUI/GUI.java
+++ Symposium/src/chessGUI/GUI.java
@@ -1,8 +1,21 @@
 package chessGUI;
 
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+
+import chessBoard.ChessBoard;
 
 public class GUI {
 
@@ -14,14 +27,44 @@ public class GUI {
 		JFrame f = new JFrame();
 		f.setVisible(true);
 		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		f.setUndecorated(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel chessBoard = new JPanel(); 
-		for(int i = 0; i< 65; i++)
+
+		JPanel chessBoard = new JPanel(new GridBagLayout() );
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		for(int i = 1; i< 82; i++)
 		{
-			chessBoard.add( new ImageIcon(null));
+			c.gridy = i%9;
+			c.gridx = i/9;
+			if(c.gridx == 0)
+			{
+				JLabel alpha = new JLabel(ChessBoard.col[c.gridy]);
+				chessBoard.add(alpha);
+			}
+			else
+			{
+				if(c.gridy == 0)
+				{
+					JLabel num = new JLabel(Integer.toString(c.gridx-1));
+					chessBoard.add(num);
+				}
+				else
+				{
+					JLabel tile = new JLabel(new ImageIcon()); // add iamge later
+
+					chessBoard.add(tile);
+				}
+			}
 		}
+		f.add(chessBoard);
+
+		String[] colNames = {"Turn","White Action", "Black Action", "Points"};
+		JTable scoreBoard = new JTable(null, colNames);
+		scoreBoard.setVisible(true);
+		f.add(scoreBoard);
+		
+		
 	}
 
 }
