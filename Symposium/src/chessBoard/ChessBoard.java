b703cafa9d1316c0dd13b11065aad9e71e4e0f6e
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-04-20 14:22:51 -0400

    Images seperated

--- Symposium/src/chessBoard/ChessBoard.java
+++ Symposium/src/chessBoard/ChessBoard.java
@@ -5,6 +5,7 @@ public class ChessBoard {
 	BoardTile[][] board = new BoardTile[8][8];
 	public static final String[] col = {"A", "B", "C", "D", "E", "F", "G", "H"};
 	String[] chessPieces = {"Rook", "Knight", "Bishop", "Queen", "King"};
+	String[] chessAddress = {};
 	public ChessBoard() {
 		for(int row =0; row< board.length; row++ )
 		{
