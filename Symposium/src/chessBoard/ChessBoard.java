6705eefe0e66c78274d35d089a6ddaf9a35ff762
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-05-30 15:21:44 -0400

    changes

--- Symposium/src/chessBoard/ChessBoard.java
+++ Symposium/src/chessBoard/ChessBoard.java
@@ -1,26 +1,32 @@
 package chessBoard;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.ImageIcon;
 
 public class ChessBoard {
 
 	private BoardTile[][] board = new BoardTile[8][8];
+	private int[] direction = {1,-1,1,-1};
 	public static final String[] col = {"A", "B", "C", "D", "E", "F", "G", "H"};
-	/*
 	
-	private String[] chessPieces = {"Rook", "Knight", "Bishop", "Queen", "King", "Bishop","Knight","Rook"};
+	
+	private String[] chessPieces = {"WRook", "WKnight", "WBishop", "WQueen", "WKing", "WBishop","WKnight","WRook"};
+	private String[] chessPiecesB = {"BRook", "BKnight", "BBishop", "BQueen", "BKing", "BBishop","BKnight","BRook"};
 	private ImageIcon[] chessImages = {new ImageIcon("Images/Rook.png"), new ImageIcon("Images/Knight.png"), new ImageIcon("Images/Bishop.png")
 			, new ImageIcon("Images/Queen.png"), new ImageIcon("Images/King.png"),new ImageIcon("Images/Bishop.png"),
 			new ImageIcon("Images/Knight.png"), new ImageIcon("Images/Rook.png")};
 	private ImageIcon[] BChessImages = {new ImageIcon("Images/BRook.png"), new ImageIcon("Images/BKnight.png"), new ImageIcon("Images/BBishop.png")
 			, new ImageIcon("Images/BQueen.png"), new ImageIcon("Images/BKing.png"),new ImageIcon("Images/BBishop.png"),
 			new ImageIcon("Images/BKnight.png"), new ImageIcon("Images/BRook.png")};
-	private ImageIcon[] tileFile = {new ImageIcon("Images/White Tile.png"), new ImageIcon("Images/Black Tile.png")};*/
+	private ImageIcon[] tileFile = {new ImageIcon("Images/White Tile.png"), new ImageIcon("Images/Black Tile.png")};
 
 	public ChessBoard() {
-		try {
+		/*try {
 			FileInputStream fileIn = new FileInputStream("Data/Board.ser");
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			board = (BoardTile[][]) in.readObject();
@@ -32,8 +38,8 @@ public class ChessBoard {
 		} catch (ClassNotFoundException c) {
 			System.out.println("Board not found");
 			c.printStackTrace();
-			return;
-		}/*
+			return;*/
+		
 		int i = 0;
 		for(int row =0; row< board.length; row++ )
 		{
@@ -53,34 +59,132 @@ public class ChessBoard {
 		}
 		populateBoard();
 		giveMov();
-		writeChessTiles();*/
+		writeChessTiles();
 	}
 	
 	public BoardTile[] viableMoves(BoardTile current) //think about the process current: rook (have boolean for direction)
 	{
-		BoardTile[] viableInputs;
+		BoardTile[] viableInputs = null;
 		if(current.getPiece().getPieceType().equals("Rook")) //check if there is anything piece blocking movements
 		{
-			String[][] possibleMoves = current.getRookMovements();
-			viableInputs = new BoardTile[possibleMoves.length];
-			int currentIndex =0;
-			for(int i =0; i< viableInputs.length; i++)
+			 viableInputs = rookVia(current.getRookMovements(), current.getCol(), current.getRow(), current.getPiece().getPieceType().substring(0, 1));
+		}
+		else
+		{
+			if(current.getPiece().getPieceType().equals("Knight"))
 			{
-				if(board[cIndexOf(possibleMoves[i][0])][Integer.parseInt(possibleMoves[i][1])].getPiece() == null)
-				{
-					viableInputs[currentIndex] = board[cIndexOf(possibleMoves[i][0])][Integer.parseInt(possibleMoves[i][1])];
-					currentIndex ++;
-				}
-				else
-				{
-					changeDirection();
-				}
+				
 			}
 		}
 		
-		return 
+		return viableInputs;
 	}
 	
+	public BoardTile[] rookVia(String[][] possibleMoves, String currentCol, int currentRow, String check)
+	{
+		int currentIndex =0;
+		BoardTile[] right = checkRight(possibleMoves, currentCol, currentIndex,check);
+		currentIndex += col[7].compareTo(currentCol);
+		BoardTile[] left = checkLeft( possibleMoves, currentCol, currentIndex,check);
+		currentIndex += currentCol.compareTo(col[0]);
+		BoardTile[] top = checkTop( possibleMoves, currentRow, currentIndex,check);
+		currentIndex += currentRow;
+		BoardTile[] bottom = checkBottom(possibleMoves, currentRow, currentIndex, check);
+		BoardTile[] viableInputs = append4Arrays(right, left, top, bottom);
+		return viableInputs;
+	}
+	
+	private BoardTile[] append4Arrays(BoardTile[] arr1, BoardTile[] arr2, BoardTile[] arr3, BoardTile[] arr4) {
+		int length = arr1.length + arr2.length + arr3.length + arr4.length;
+		BoardTile[] ans = new BoardTile[length];
+		
+		int currentIdx =0;
+		
+		for( BoardTile e : arr1) {
+			ans[currentIdx] = e;
+			currentIdx ++;
+		}
+		
+		for( BoardTile e: arr2) {
+			ans[currentIdx] = e;
+			currentIdx ++;
+		}
+		for( BoardTile e: arr3) {
+			ans[currentIdx] = e;
+			currentIdx ++;
+		}
+		for( BoardTile e: arr4) {
+			ans[currentIdx] = e;
+			currentIdx ++;
+		}
+		
+		return ans;
+	}
+
+	private BoardTile[] checkBottom(String[][] possibleMoves, int currentRow, int startIdx, String check) {
+		BoardTile[] ans = new BoardTile[possibleMoves.length];
+		for(int i = 0; i < 8-currentRow; i++){
+			if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece() == null)
+				ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+			else{
+				if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece().getPieceType().compareTo(check) != 0){
+					ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+					break;
+				}
+				else break;
+			}
+		}
+		return ans;
+	}
+
+	private BoardTile[] checkTop(String[][] possibleMoves, int currentRow, int startIdx, String check) {
+		BoardTile[] ans = new BoardTile[possibleMoves.length];
+		for(int i = 0; i < currentRow; i++){
+			if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece() == null)
+				ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+			else{
+				if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece().getPieceType().compareTo(check) != 0){
+					ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+					break;
+				}
+				else break;
+			}
+		}
+		return ans;
+	}
+
+	private BoardTile[] checkLeft(String[][] possibleMoves, String currentCol, int startIdx, String check) {
+		BoardTile[] ans = new BoardTile[possibleMoves.length];
+		for(int i = 0; i<currentCol.compareTo(col[0]); i++){
+			if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece() == null)
+				ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+			else{
+				if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece().getPieceType().compareTo(check) != 0){
+					ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+					break;
+				}
+				else break;
+			}
+		}
+		return ans;
+	}
+	
+	private BoardTile[] checkRight(String[][] possibleMoves, String currentCol, int startIdx, String check) {
+		BoardTile[] ans = new BoardTile[possibleMoves.length];
+		for(int i = 0; i<col[7].compareTo(currentCol); i++){
+			if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece() == null)
+				ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+			else{
+				if(board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])].getPiece().getPieceType().compareTo(check) != 0){
+					ans[i] = board[cIndexOf(possibleMoves[startIdx + i][0])][Integer.parseInt(possibleMoves[startIdx + i][1])];
+					break;
+				}
+				else break;
+			}
+		}
+		return ans;
+	}
+
 	public int cIndexOf(String alpha)
 	{
 		for(int i= 0; i< col.length; i++)
@@ -92,10 +196,10 @@ public class ChessBoard {
 		}
 		return -1;
 	}
-/*
+
 	private void writeChessTiles() {
 		try {
-			FileOutputStream fileOut = new FileOutputStream("Board.ser");
+			FileOutputStream fileOut = new FileOutputStream("Data/Board.ser");
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 			out.writeObject(board);
 			out.close();
@@ -118,7 +222,7 @@ public class ChessBoard {
 				}
 				if(row == 6)
 				{
-					board[row][col].setChessPiece(new ChessPiece("Pawn", new ImageIcon("Images/BPawn.png")));
+					board[row][col].setChessPiece(new ChessPiece("BPawn", new ImageIcon("Images/BPawn.png")));
 				}
 				if(row == 0)
 				{
@@ -126,7 +230,7 @@ public class ChessBoard {
 				}
 				if(row == 7)
 				{
-					board[row][col].setChessPiece(new ChessPiece(chessPieces[col], BChessImages[col]));
+					board[row][col].setChessPiece(new ChessPiece(chessPiecesB[col], BChessImages[col]));
 				}
 
 			}
@@ -159,7 +263,7 @@ public class ChessBoard {
 	}
 	private String[][] bpawn(BoardTile current) {
 		String[][] temp = new String[3][2];
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		int tempRow =0;
 		if( current.getRow() -1 >= 0)
 		{
@@ -186,7 +290,7 @@ public class ChessBoard {
 
 	private String[][] wpawn(BoardTile current) {
 		String[][] temp = new String[3][2];
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		int tempRow =0;
 		if( current.getRow() -1 > 0)
 		{
@@ -215,7 +319,7 @@ public class ChessBoard {
 
 	private String[][] knight(BoardTile current) {
 		String[][] temp = new String[8][2];
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		int tempRow =0;
 
 		if(alphaIndex - 1 > 0)
@@ -283,7 +387,7 @@ public class ChessBoard {
 
 	private String[][] rook(BoardTile current) {
 		String[][] temp = new String[14][2];
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		int tempRow =0;
 		if(alphaIndex < board.length)
 		{
@@ -327,7 +431,7 @@ public class ChessBoard {
 	public String[][] queen(BoardTile current)
 	{
 		String[][] temp = new String[27][2];
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		int tempRow =0;
 		if(alphaIndex < board.length)
 		{
@@ -407,7 +511,7 @@ public class ChessBoard {
 	public String[][] king(BoardTile current)
 	{
 		String[][] temp = new String[8][2];
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		int tempRow =0;
 		if(alphaIndex - 1 > 0)
 		{
@@ -464,7 +568,7 @@ public class ChessBoard {
 
 		String[][] temp = new String[13][2];
 		int tempRow = 0;
-		int alphaIndex = CindexOf(current.getCol());
+		int alphaIndex = cIndexOf(current.getCol());
 		for(int i = 1; i<8; i++)
 		{
 			if( alphaIndex- i> 0)
@@ -505,13 +609,12 @@ public class ChessBoard {
 		return temp;
 	}
 
-	public void move(ChessPiece piece, BoardTile target, BoardTile previous)
-	{
+	public void move(ChessPiece piece, BoardTile target, BoardTile previous){
 		target.setChessPiece(piece);
 		previous.setChessPiece(null);
 	}
-	*/
-	public BoardTile[][] getBoard() {
+	
+	public BoardTile[][] getBoard(){
 		return board;
 	}
 }
